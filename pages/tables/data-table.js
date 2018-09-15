b5cf7b6feffd9c2f4a2900b78ee7da14a35d35db
rahul-dhakade <rahul10.pd@gmail.com>
2018-09-15 15:20:25 +0530

    initial commit

---
+++ pages/tables/data-table.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $('#regClients').DataTable( {
+        dom: 'Bfrtip',
+        buttons: [
+            'copy', 'csv', 'excel', 'pdf', 'print'
+        ]
+    } );
+} );
